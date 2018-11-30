eb52c5e3095fc73d2ddebb90e24b15b11c088e7c
Javon Harper <javon.d.harper@gmail.com>
2018-11-29 20:14:36 -0500

    Add redux persist

--- packages/app/App.js
+++ packages/app/App.js
@@ -2,7 +2,12 @@ import React, { Component } from 'react';
 import { View, StyleSheet } from 'react-native';
 import { createStackNavigator } from 'react-navigation';
 import { applyMiddleware, createStore } from 'redux';
-import promiseMiddleware from 'redux-promise-middleware'
+
+// Combine these
+import { persistStore, persistReducer } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
+import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web and AsyncStorage for react-native
+
 import { createLogger } from 'redux-logger';
 import { Provider } from 'react-redux';
 import NativeTachyons from 'react-native-style-tachyons';
@@ -45,14 +50,24 @@ const Navigation = createStackNavigator(
   }
 );
 
-const logger = createLogger({});
+const persistConfig = {
+  key: 'root',
+  storage
+};
 
-const store = createStore(reducer, applyMiddleware(logger, promiseMiddleware()));
-const navigationPersistenceKey = __DEV__ ? 'XXXXXXX' : null;
+const persistedReducer = persistReducer(persistConfig, reducer);
+
+const logger = createLogger({});
+const store = createStore(persistedReducer, applyMiddleware(logger));
+// const navigationPersistenceKey = __DEV__ ? 'XXXXXXX' : null;
+
+const persistor = persistStore(store);
 
 const Root = () => (
   <Provider store={store}>
-    <Navigation persistenceKey={navigationPersistenceKey} />
+    <PersistGate loading={null} persistor={persistor}>
+      <Navigation />
+    </PersistGate>
   </Provider>
 );
 
