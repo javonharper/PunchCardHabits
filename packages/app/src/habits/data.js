e53ecb5626215e3a4ccf97523856c1ffce344b8a
Javon Harper <javon.d.harper@gmail.com>
2018-12-01 12:44:10 -0500

    Prepare for release

--- packages/app/src/habits/data.js
+++ packages/app/src/habits/data.js
@@ -28,54 +28,7 @@ export const deleteHabit = habitId => ({
   habitId
 });
 
-const habitId1 = uuid();
-const habitId2 = uuid();
-const today = moment().format();
-
-const initialState = {
-  completions: [
-    {
-      date: moment(today).format('YYYY-MM-DD'),
-      timestamp: moment(today).format(),
-      habitId: habitId1
-    },
-    {
-      date: moment(today).format('YYYY-MM-DD'),
-      timestamp: moment(today).format(),
-      habitId: habitId2
-    },
-    {
-      date: moment(today)
-        .subtract(1, 'days')
-        .format('YYYY-MM-DD'),
-      timestamp: moment(today).format(),
-      habitId: habitId2
-    },
-    {
-      date: moment(today)
-        .subtract(9, 'days')
-        .format('YYYY-MM-DD'),
-      timestamp: moment(today).format(),
-      habitId: habitId2
-    }
-  ],
-  habits: [
-    {
-      id: habitId1,
-      name: 'Meditate',
-      goal: 2,
-      frequency: FREQUENCY.DAILY,
-      color: palette.purple
-    },
-    {
-      id: habitId2,
-      name: 'Go to the gym',
-      goal: 3,
-      frequency: FREQUENCY.WEEKLY,
-      color: palette.red
-    }
-  ]
-};
+const initialState = {};
 
 export const reducer = (state = initialState, action) => {
   switch (action.type) {
